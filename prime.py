3e2c834f2ce30f5b9116ef7961aea856294ec238
Atharva <atharvavedante95@gmail.com>
2026-07-30 14:46:58 +0530

    2st commit 2

--- prime.py
+++ prime.py
@@ -1,25 +1,14 @@
 import math
 
 num = int(input("Enter a number: "))
-
 root = int(math.sqrt(num))
 
-if root * root != num:
-    print("Square root is not a whole number")
+c = 0
+for i in range(1, root + 1):
+    if root % i == 0:
+        c += 1
+
+if c == 2:
+    print("Square root is Prime")
 else:
-    prime = True
-
-    if root < 2:
-        prime = False
-    else:
-        for i in range(2, root):
-            if root % i == 0:
-                prime = False
-                break
-
-    if prime:
-        print("Square root =", root)
-        print("Square root is Prime")
-    else:
-        print("Square root =", root)
-        print("Square root is Not Prime")
+    print("Square root is Not Prime")
