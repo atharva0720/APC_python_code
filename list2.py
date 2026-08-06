cbb7d36aec7640cd07054faa17acf3ba649195e3
Atharva <atharvavedante95@gmail.com>
2026-08-06 14:34:36 +0530

    list prog 2

--- list2.py
+++ list2.py
@@ -2,4 +2,12 @@ l1=[10,20,30,40,50]
 
 for i in l1:
     print(i)
-    
+
+print("Display 1st element")
+print(l1[0])
+
+print("Dipslay Last element")
+print(l1[-1])
+
+print("thrid pos elemt")
+print(l1[2])
---
+++ list3_colr_change.py
@@ -0,0 +1,12 @@
+l=['red','blue','white','black','orange']
+
+print('colors list')
+for i in l:
+    print(i)
+
+print("Updating 3 rd color with pink")
+
+l[2]='pink'
+
+for i in l:
+    print(i)
