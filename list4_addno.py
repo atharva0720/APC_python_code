ed50dd114261020bf61aa20cfea7fa962a099113
Atharva <atharvavedante95@gmail.com>
2026-08-11 10:16:19 +0530

    All

---
+++ list4_addno.py
@@ -0,0 +1,13 @@
+l=[10,20,30,40,50,60]
+
+print('display all elements of list')
+for i in l:
+    print(i)
+
+print('Add elements in last of the list')
+l.append(70)
+for i in l:
+    print(i)
+
+print('add elements at 1st position')
+l.add(100)
