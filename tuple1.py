197f673d877d2de206d07403950752e3e4466ccf
Atharva <atharvavedante95@gmail.com>
2026-08-13 14:10:05 +0530

    tuple

---
+++ tuple1.py
@@ -0,0 +1,3 @@
+# Q1. Create a tuple of five integers and display it.
+t = (10, 20, 30, 40, 50)
+print(t)
---
+++ tuple2.py
@@ -0,0 +1,5 @@
+# Q2. Create a tuple containing five city names. Display first, last and third city.
+cities = ("Kolhapur", "Pune", "Mumbai", "Nashik", "Nagpur")
+print("First City:", cities[0])
+print("Last City:", cities[-1])
+print("Third City:", cities[2])
